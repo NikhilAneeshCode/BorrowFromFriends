918a5b07e9729fc126ed893f16f276163dd721a4
Nikhil Khanna <nikhilkh@gmail.com>
2014-02-09 16:19:58 -0800

    Initial Commit

---
+++ Borrow From Friends/BFFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BFFAppDelegate.h
+//  Borrow From Friends
+//
+//  Created by Nikhil Khanna on 2/9/14.
+//  Copyright (c) 2014 Nikhil and Aneesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BFFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Borrow From Friends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Borrow From Friends
+//
+//  Created by Nikhil Khanna on 2/9/14.
+//  Copyright (c) 2014 Nikhil and Aneesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BFFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BFFAppDelegate class]));
+    }
+}
