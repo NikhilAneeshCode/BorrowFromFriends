426a39e02c2b1d6f2e4af6a77342803bc110b9b7
Nikhil Khanna <nikhilkh@gmail.com>
2014-02-16 12:37:42 -0800

    Embed main app flow in nag controller and add “add item view controller”

---
+++ Borrow From Friends/BFFAddItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  BFFAddItemViewController.h
+//  Borrow From Friends
+//
+//  Created by Nikhil Khanna on 2/16/14.
+//  Copyright (c) 2014 Nikhil and Aneesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+@interface BFFAddItemViewController : UIViewController
+
+@end
---
+++ Borrow From Friends/BFFAddItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  BFFAddItemViewController.m
+//  Borrow From Friends
+//
+//  Created by Nikhil Khanna on 2/16/14.
+//  Copyright (c) 2014 Nikhil and Aneesh. All rights reserved.
+//
+
+#import "BFFAddItemViewController.h"
+
+@interface BFFAddItemViewController ()
+
+@end
+
+@implementation BFFAddItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
