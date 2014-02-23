450faa9e7b3250dbb86183ebea2ea438f46eb15a
Nikhil Khanna <nikhilkh@gmail.com>
2014-02-22 19:27:56 -0800

    Add fallback feed dialog (with no tagging) when Facebook app not installed

--- Borrow From Friends/BFFTransactionDetailViewController.m
+++ Borrow From Friends/BFFTransactionDetailViewController.m
@@ -174,9 +174,55 @@
                                                      }];
             
             // If the Facebook app is NOT installed and we can't present the share dialog
-        } else {
-            // FALLBACK GOES HERE
+        }
+        else
+        {
+            NSMutableDictionary *params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                           title, @"name",
+                                           @"http://i.imgur.com/g3Qc1HN.png", @"picture",
+                                           nil];
+            // Show the feed dialog
+            [FBWebDialogs presentFeedDialogModallyWithSession:nil
+                                                   parameters:params
+                                                      handler:^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+                                                          if (error) {
+                                                              // Error launching the dialog or publishing a story.
+                                                              NSLog([NSString stringWithFormat:@"Error publishing story: %@", error.description]);
+                                                          } else {
+                                                              if (result == FBWebDialogResultDialogNotCompleted) {
+                                                                  // User cancelled.
+                                                                  NSLog(@"User cancelled.");
+                                                              } else {
+                                                                  // Handle the publish feed callback
+                                                                  NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
+                                                                  
+                                                                  if (![urlParams valueForKey:@"post_id"]) {
+                                                                      // User canceled.
+                                                                      NSLog(@"User cancelled.");
+                                                                      
+                                                                  } else {
+                                                                      // User clicked the Share button
+                                                                      NSString *result = [NSString stringWithFormat: @"Posted story, id: %@", [urlParams valueForKey:@"post_id"]];
+                                                                      NSLog(@"result %@", result);
+                                                                  }
+                                                              }
+                                                          }
+                                                      }];
         }
     
+}
+/**
+ * A function for parsing URL parameters.
+ */
+- (NSDictionary*)parseURLParams:(NSString *)query {
+    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    for (NSString *pair in pairs) {
+        NSArray *kv = [pair componentsSeparatedByString:@"="];
+        NSString *val =
+        [kv[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        params[kv[0]] = val;
+    }
+    return params;
 }
 @end
