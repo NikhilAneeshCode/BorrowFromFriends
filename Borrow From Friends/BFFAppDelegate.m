0cfb7a312eee7a245f122ec8140e070a48dd6a64
Nikhil Khanna <nikhilkh@gmail.com>
2014-03-22 12:24:35 -0700

    Change Bundle ID

--- Borrow From Friends/BFFAppDelegate.m
+++ Borrow From Friends/BFFAppDelegate.m
@@ -29,7 +29,7 @@
     //TO DO CALL APPIRATER DID SOMETHING SIGNIFICANT WHEN NECCESSARY
     //note about appirater: call [Appirater userDidSignificantEvent:YES] when the user does something significant
     [Appirater setAppId:@"558312836"];//temporary app id TODO SWITCH THIS TO REAL APP ID
-    [Appirater setDaysUntilPrompt:1];
+    [Appirater setDaysUntilPrompt:2];
     [Appirater setUsesUntilPrompt:2];
     [Appirater setSignificantEventsUntilPrompt:3];
     [Appirater setTimeBeforeReminding:2];
